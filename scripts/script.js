b329bfec205bf208a6e579322f19741651608505
Julbertina <sakal.iuliia@gmail.com>
2022-10-31 18:31:51 +0100

    Adjusment

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
 const modalButton = document.getElementById("modalButton");
 const closePopup = document.getElementById("closePopup");
 const modal = document.getElementById("modal");
+const modalConsult = document.getElementById("modalConsult");
+const sendButton = document.getElementById("sendButton");
 
 modalButton.addEventListener("click", function () {
 
@@ -11,5 +13,17 @@ modalButton.addEventListener("click", function () {
 closePopup.addEventListener("click", function () {
 
     modal.classList.remove("popup-show")
+    
+})
+
+sendButton.addEventListener("click", function () {
+
+    modal.classList.remove("popup-show")
+    
+})
+
+modalConsult.addEventListener("click", function () {
+
+    modal.classList.add("popup-show")
     
 })
